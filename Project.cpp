ea758f166cfec44a429fb831b7644da125eefd22
André Cardoso <marquescardoso@ua.pt>
2018-12-17 15:55:07 +0000

    frist commit

---
+++ Project.cpp
@@ -0,0 +1,12 @@
+// Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "opencv2/core/core.hpp"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
